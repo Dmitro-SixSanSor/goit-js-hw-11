3b57ff717ceeba2924f4e1e542991413960d4a74
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-03-04 09:11:49 +0200

    add markup header

--- src/main.js
+++ src/main.js
@@ -1,31 +1,53 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { fetchImages } from './js/pixabay-api';
 import { renderGallery } from './js/render-functions';
 
 export const refs = {
   form: document.querySelector('.search-form'),
-  input: document.querySelector('#search-text'), 
+  input: document.querySelector('#search-text'),
   button: document.querySelector('button[type="submit"]'),
-  gallery: document.querySelector('.gallery')
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader-box') 
 };
 
-refs.form.addEventListener('submit', e => {
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function toggleLoader() {
+  refs.loader.classList.toggle('hidden');
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+if (refs.form) {
+  refs.form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const query = refs.input.value.trim();
 
-  if (query === '') {
-    iziToast.error({
-      title: 'Error',
-      message: 'Please enter a search query.',
-      position: 'topRight',
-    });
-    return;
-  }
+    if (query === '') {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please enter a search query.',
+        position: 'topRight',
+      });
+      return;
+    }
 
-  fetchImages(query)
-    .then(data => {
+    clearGallery(); 
+    toggleLoader();
+
+    try {
+      const data = await fetchImages(query);
+      
       if (data.hits.length === 0) {
         iziToast.info({
           title: 'No Results',
@@ -33,16 +55,25 @@ refs.form.addEventListener('submit', e => {
           position: 'topRight',
         });
       } else {
-        const markup = imgsTemplate(data.hits);
+        const markup = renderGallery(data.hits);
         refs.gallery.innerHTML = markup;
+
+        lightbox.refresh();
       }
-    })
-    .catch(error => {
+    } catch (error) {
       iziToast.error({
         title: 'Error',
         message: 'Something went wrong. Please try again.',
         position: 'topRight',
       });
-    });
-  e.target.reset();
-});
+    } finally {
+      toggleLoader(); 
+    }
+
+    e.target.reset();
+  });
+} else {
+  console.error("Форма не знайдена в DOM!");
+}
+
+export { clearGallery, toggleLoader, lightbox };
