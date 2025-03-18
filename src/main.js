620f7dd753a414775e658fd6f557f545fcd34656
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-03-18 22:36:33 +0200

    add markup header

--- src/main.js
+++ src/main.js
@@ -1,10 +1,50 @@
-import { onSubmit } from './js/pixabay-api.js';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { searchImg } from './js/pixabay-api.js';
+import { imgsTemplate, lightbox } from './js/render-functions.js';
 
 export const refs = {
-  searchBtn: document.querySelector('.js-search-btn'),
-  searchInput: document.querySelector('.js-search-field'),
-  form: document.querySelector('.js-search-form'),
-  gallery: document.querySelector('.loader'),
+    form: document.querySelector('.search-form'),
+    input: document.querySelector('#image-input'),
+    gallery: document.querySelector('.gallery')
 };
 
-refs.form.addEventListener('submit', onSubmit);
+refs.form.addEventListener('submit', e => {
+    e.preventDefault();
+    const query = refs.input.value.trim();
+
+  if (query === '') {
+    return;
+  }
+  refs.gallery.innerHTML = '<span class="loader"></span>';
+  refs.form.reset();
+
+  searchImg(query)
+    .then(({ data }) => {
+      refs.gallery.innerHTML = '';
+      
+      if (data.hits.length === 0) {
+        iziToast.info({
+          title: '',
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: '#fafafb',
+          backgroundColor: '#ef4040',
+          messageSize: '16px',
+          position: 'topRight',
+          maxWidth: '432px',
+        });
+      } else {
+        refs.gallery.innerHTML = imgsTemplate(data.hits);
+        lightbox.refresh();
+      }
+    })
+    .catch(error => {
+       refs.gallery.innerHTML = '';
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Please try again.',
+        position: 'topRight',
+      });
+    });
+});
