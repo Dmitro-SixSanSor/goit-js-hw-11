87999ac8a34a41b12a9482795f190dcfb1c34cd2
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-03-18 22:32:33 +0200

    add markup header

--- src/main.js
+++ src/main.js
@@ -1,66 +1,10 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
-import { fetchImages } from './js/pixabay-api';
-import { renderGallery } from './js/render-functions';
+import { onSubmit } from './js/pixabay-api.js';
 
 export const refs = {
-  form: document.querySelector('.search-form'),
-  input: document.querySelector('#search-text'),
-  button: document.querySelector('button[type="submit"]'),
-  gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.loader') // виправив
+  searchBtn: document.querySelector('.js-search-btn'),
+  searchInput: document.querySelector('.js-search-field'),
+  form: document.querySelector('.js-search-form'),
+  gallery: document.querySelector('.loader'),
 };
 
-function toggleLoader() {
-  refs.loader.classList.toggle('hidden');
-}
-
-function clearGallery() {
-  refs.gallery.innerHTML = '';
-}
-
-if (refs.form) {
-  refs.form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const query = refs.input.value.trim();
-
-    if (query === '') {
-      iziToast.error({
-        title: 'Error',
-        message: 'Please enter a search query.',
-        position: 'topRight',
-      });
-      return;
-    }
-
-    clearGallery(); 
-    toggleLoader();
-
-    try {
-      const data = await fetchImages(query);
-      
-      if (data.hits.length === 0) {
-        iziToast.info({
-          title: 'No Results',
-          message: 'No images found for your search.',
-          position: 'topRight',
-        });
-      } else {
-        renderGallery(data.hits, refs.gallery);
-      }
-    } catch (error) {
-      iziToast.error({
-        title: 'Error',
-        message: 'Something went wrong. Please try again.',
-        position: 'topRight',
-      });
-    } finally {
-      toggleLoader(); 
-    }
-
-    e.target.reset();
-  });
-} else {
-  console.error("Форма не знайдена в DOM!");
-}
+refs.form.addEventListener('submit', onSubmit);
