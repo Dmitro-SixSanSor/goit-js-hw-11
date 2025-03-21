313c7de6c41f189edbf965073129dd430533e97f
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-03-21 10:50:21 +0200

    add markup header

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,18 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector(`.gallery`);
-export function renderImages(images){
-    gallery.innerHTML = images
+const gallery = document.querySelector('.gallery');
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function renderImages(images) {
+  clearGallery();
+
+  const markup = images
     .map(
       ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
       <li class="gallery-item">
@@ -11,30 +20,30 @@ export function renderImages(images){
           <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
         </a>
         <div class="info">
-        <ul class="baner">
-          <li class="baner-li">
-            <p class="baner-title">Likes</p>
-            <p class="baner-text">${likes}</p>
-          </li>
-          <li class="baner-li">
-            <p class="baner-title">Views</p>
-            <p class="baner-text">${views}</p>
-          </li>
-          <li class="baner-li">
-            <p class="baner-title">Comments</p>
-            <p class="baner-text">${comments}</p>
-          </li>
-          <li class="baner-li">
-            <p class="baner-title">Downloads</p>
-            <p class="baner-text">${downloads}</p>
-          
+          <ul class="baner">
+            <li class="baner-li">
+              <p class="baner-title">Likes</p>
+              <p class="baner-text">${likes}</p>
             </li>
-        </ul>
+            <li class="baner-li">
+              <p class="baner-title">Views</p>
+              <p class="baner-text">${views}</p>
+            </li>
+            <li class="baner-li">
+              <p class="baner-title">Comments</p>
+              <p class="baner-text">${comments}</p>
+            </li>
+            <li class="baner-li">
+              <p class="baner-title">Downloads</p>
+              <p class="baner-text">${downloads}</p>
+            </li>
+          </ul>
         </div>
       </li>`
     )
     .join('');
 
-  const lightbox = new SimpleLightbox('.gallery a');
+  gallery.innerHTML = markup; ё
+
   lightbox.refresh();
 }
--- src/main.js
+++ src/main.js
@@ -1,37 +1,53 @@
-import { renderImages } from "./js/render-functions";
-import { fetchImages } from "./js/pixabay-api";
+import { renderImages, clearGallery } from './js/render-functions';
+import { fetchImages } from './js/pixabay-api';
 
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector(`form`);
-const gallery = document.querySelector(`.gallery`);
-const loader = document.querySelector(`.loader`);
+const form = document.querySelector('form');
+const loader = document.querySelector('.loader');
 
-form.addEventListener(`submit`, async(event)=>{
-    event.preventDefault();
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-    const query = event.target.elements[`search-text`].value.trim();
-    if (!query){
-        iziToast.error({title: `Error`, message: `Please enter a search query!`});
-        return;
+  const query = event.target.elements['search-text'].value.trim();
+  if (!query) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const images = await fetchImages(query);
+
+    if (images.length === 0) {
+      iziToast.warning({
+        title: 'Oops!',
+        message: 'No images found. Try again!',
+      });
+    } else {
+      renderImages(images);
     }
-
-    gallery.innerHTML = ``;
-    loader.classList.add(`visible`);
-    try{
-        const images = await fetchImages(query);
-        console.log(images);
-        
-        if(images.length === 0){
-            iziToast.warning({title: `Oops!`, message: `No images found. Try again!`});
-        }else{
-            renderImages(images);
-        }
-    }catch(error){
-        iziToast.error({title: `Error`, message: ` Failed to fetch images. Try again later!`});
-    }finally{
-        loader.classList.remove(`visible`);
-    }
-
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to fetch images. Try again later!',
+    });
+  } finally {
+    hideLoader();
+  }
 });
+
+function showLoader() {
+  loader.classList.add('visible');
+}
+
+function hideLoader() {
+  loader.classList.remove('visible');
+}
+
